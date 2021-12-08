91f3f1878477eb0d1d457f48fcf6772e3fb472d5
Carlos Colombo <csc7@hw.ac.uk>
2021-12-08 23:54:15 +0000

    Move content down in Weather page when clicking on the Bootstrap hamburger menu

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,8 +1,17 @@
-
-
-
-
-
+// Move content down when clicking the Bootstrap hamburguer
+// button in a fixed navigation menu
+// Copied from my own previous PP2_CI_WW Project
+let hamburguerButton = document.getElementsByTagName("button")[0];
+hamburguerButton.addEventListener("click", moveContent);
+function moveContent () {
+    if (document.getElementsByClassName("navbar-toggler")[0].getAttribute("aria-expanded") == "false") {
+        let firstElementToMove = document.getElementById('top-container');
+        firstElementToMove.style.marginTop = "250px";
+    } else {
+        let firstElementToMove = document.getElementById('top-container');
+        firstElementToMove.style.marginTop = "75px";
+    }
+}
 
 
 
