004a4906d2d6ec2225c6aa829b1d3c7566d22d5a
Carlos Colombo <csc7@hw.ac.uk>
2021-12-29 00:43:15 +0000

    Add weather models to admin management

--- app_weather/admin.py
+++ app_weather/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import WindData, TemperatureData, OtherWeatherData
 
 # Register your models here.
+
+admin.site.register(WindData)
+admin.site.register(TemperatureData)
+admin.site.register(OtherWeatherData)
