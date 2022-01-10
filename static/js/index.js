35ab56ed63bb686d3fc796e65bf5c122cd37bcb6
Carlos Colombo <csc7@hw.ac.uk>
2022-01-10 11:16:13 +0000

    Write instructions for weather page in corresponding page, including images and style

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 // Wait for the page to be loaded
 // Credit: Code Institute
 $(document).ready(function() {
-    $("#instructions-title > div > div > p").append(" - PAGE LOADED");
+    $("#instructions-title > div > div > p").css("color", "rgb(76, 76, 211)");
 });
 
