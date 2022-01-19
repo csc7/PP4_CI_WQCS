8e33ab9e914562ceb999a6ef89e55e024ce5ccdf
Carlos Colombo <csc7@hw.ac.uk>
2022-01-19 19:39:27 +0000

    Add test for Post form in app_blog

--- app_blog/tests.py
+++ app_blog/tests.py
@@ -23,35 +23,60 @@ class TestCommentForm(unittest.TestCase):
     Class for testing the contact form
     """
 
-    
+
     def setUp(self):
         print("setUp")
-        self.test_form_data_to_test = Comment(            
+        self.test_data_for_comment = Comment(            
             False,            
             True,
             'Jkl',
             'Ghi@ghi.com',
             'Body text of comment'
         )
+        self.test_data_for_post = Post(            
+            'postpost',
+            'Title',
+            'slug-example',
+            100,
+            'placeholder',
+            True,
+            True,
+            'post content',
+            True,
+            0
+        )
 
     def tearDown(self):
         print("tearDown")
 
+    def test_comment_class_return(self):
+        print("Testing class return in comment")
+        self.assertEqual(str(self.test_data_for_comment), 'Comment Body text of comment by Jkl')
 
     def test_name_in_comment(self):
         print("Testing name in comment")
-        self.assertEqual(self.test_form_data_to_test.name, 'Jkl')
+        self.assertEqual(self.test_data_for_comment.name, 'Jkl')
 
     def test_email_in_comment(self):
         print("Testing email in comment")
-        self.assertEqual(self.test_form_data_to_test.email, 'Ghi@ghi.com')
+        self.assertEqual(self.test_data_for_comment.email, 'Ghi@ghi.com')
 
     def test_body_in_comment(self):
         print("Testing body in comment")
-        self.assertEqual(self.test_form_data_to_test.body,
+        self.assertEqual(self.test_data_for_comment.body,
                          'Body text of comment'
                          )
+    def test_post_class_return(self):
+        print("Testing class return in post")
+        self.assertEqual(str(self.test_data_for_post), 'Title')
+    
+    def test_slug_in_post(self):
+        print("Testing slug in comment")
+        self.assertEqual(self.test_data_for_post.slug, 'slug-example')
 
+    def test_author_in_post(self):
+        print("Testing slug in comment")
+        self.assertEqual(self.test_data_for_post.content, 'post content')
 
 if __name__ == '__main__':
     unittest.main()
