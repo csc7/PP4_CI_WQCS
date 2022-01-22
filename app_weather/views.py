ec81ea8acff7c20d2ffa02da2cc41c456761f822
Carlos Colombo <csc7@hw.ac.uk>
2022-01-22 20:55:58 +0000

    Work on AJAX sending data, not working

--- app_weather/views.py
+++ app_weather/views.py
@@ -41,7 +41,7 @@ def get_weather_page(request):
         elif records_to_display == "15-last":
             recs = 15
         else:
-            recs = 100
+            recs = 30
         
         other_value_to_display_1 = str(json.dumps(request.POST.get('otherValueToDisplay1'))[1:-1])
         other_value_to_display_2 = str(json.dumps(request.POST.get('otherValueToDisplay2'))[1:-1])
@@ -49,7 +49,7 @@ def get_weather_page(request):
         print(other_value_to_display_2)
         
         # Write new record
-        if write_data == "true":
+        if write_data == True:
             print("Writing = " + write_data)
         
             # Indexes from 1 to -1 to delete quotation marks  
@@ -83,7 +83,7 @@ def get_weather_page(request):
             #print("It's AJAX")
             #print(records_to_display)
             
-            recs = 7
+            recs = 30
             print(recs)
             if records_to_display == "5-last":
                 recs = 5
@@ -153,79 +153,3 @@ def get_weather_page(request):
         }    
     
     return render(request, "weather.html", context)
-
-
-
-
-#@csrf_exempt
-#def send_weather_data(request):
-#    if request.is_ajax():
-#        wind_speed_data = (request.POST.get('value_wind'))
-#        a=float(wind_speed_data)    
-#        print(wind_speed_data)
-#        record = WindData(date='2000-01-01 12:00', wind_speed=a, wind_direction=a)
-#        record.save()
-#        context = {
-#            'wind_speed': WindData.objects.all()
-#        }
-#        #return render(request, "weather.html", context)
-
-
-
-
-
-
-#def save_result(request):
-#    table_data = json.loads(request.POST.get('wind_value'))
-#    print(table_data)
-#    return render(request, 'weather.html')
-
-
-#def new_page(request):
-#    data = request.GET['wind-speed-value']
-#    return render(request, {'data':data})
-
-
-#def weather_form(request):
-#    if request.method == "post":
-#        a = request.POST.get("wind-speed")
-#        print (a)
-#    return render(request,"weather.html", context="asdfsdf")
-
-
-#weather_data_form = request.POST
-
-
-#def get_contact_page(request):
-#    return render(request, "contact.html")
-
-
-#def post(self,request, *args, **kwargs):
-#    if self.request.is_ajax():
-#        return self.ajax(request)
-#
-#def ajax(self, request):
-#    response_dict= {
-#        'success': True,
-#    }
-#    action = request.POST.get('action','')
-#
-#    if action == 'add_car':
-#        car_id = request.POST.get('id','')
-#
-#    if hasattr(self, action):
-#        response_dict = getattr(self, action)(request)
-#        car = CAR.objects.get(ida_name='car_id')
-#        response_dict = {
-#            'car_name':car.name
-#        }
-#
-#    return HttpResponse(simplejson.dumps(response_dict),
-#                        mimetype='application/json')
-
-#def weather_data(request, id):
-#    post_id = request.GET['wd']
-#    print(post_id)
-#    #sql insertion code,once its done i want to notify to the front end..print some alert message.
-#    return render(request, "weather.html")
-
